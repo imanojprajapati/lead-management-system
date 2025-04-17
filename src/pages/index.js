05b0d9f249ab43ee75c5a2cdcc2b9aa58dc1a0c9
imanojprajapati <mnprajapati007@gmail.com>
2025-04-17 14:39:23 +0530

    Fix a Issue of Build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const router = useRouter();
